af9cd64099144879b70df33fc1dd79d2dfbc9044
wangyiwei <wangyw.dev@outlook.com>
2023-09-07 17:46:24 +0800

    init

---
+++ skwdgt/skwdgtBundle.swift
@@ -0,0 +1,17 @@
+//
+//  skwdgtBundle.swift
+//  skwdgt
+//
+//  Created by leo on 2023-07-17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct skwdgtBundle: WidgetBundle {
+    var body: some Widget {
+        skwdgt()
+        skwdgtLiveActivity()
+    }
+}
